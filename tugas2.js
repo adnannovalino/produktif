2e5eb4b550a6d436113fbc93173836f73d545ce1
adnannovalino <168519055+adnannovalino@users.noreply.github.com>
2024-05-13 14:11:47 +0700

    Create tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,27 @@
+var x="adnan";
+x+=1000;
+console.log(x)
+ 
+ var x="bagas";
+x+=1000;
+console.log(x)
+
+var x="dimas";
+x+=1000;
+console.log(x)
+
+var x="galih";
+x+=1000;
+console.log(x)
+
+var x="rehan";
+x+=1000;
+console.log(x)
+
+
+var x={nama:"adnan",umur:16,absen:1,kelas:"xrpl3"};
+console.log(x)
+console.log(x.nama)
+console.log(x.umur)
+console.log(x.absen)
+console.log(x.kelas)
