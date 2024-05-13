cf4843ac368062af26e00f7d1ed8915c7718318e
adnannovalino <168519055+adnannovalino@users.noreply.github.com>
2024-05-13 14:10:50 +0700

    Create tugas1.js

---
+++ tugas1.js
@@ -0,0 +1,43 @@
+//let//
+
+let x= 21 + 14
+ x= 10 + 11
+console.log(x)
+
+ x= 26 + 19
+ x+= 4 - 7
+console.log(x)
+
+x= 40 / 10
+ x-= 11 + 3
+console.log(x)
+
+x= 27 - 25
+ x!= 45 + 32
+console.log(x)
+
+x= 23 * 10
+ x/= 21 + 2
+console.log(x)
+
+//var//
+
+var y= 76 + 11
+var y= 67 + 10
+console.log(y)
+
+var y= 12 + 11
+var y= 39 + 10
+console.log(y)
+
+var y= 34 + 11
+var y= 37 + 10
+console.log(y)
+
+var y= 23 + 11
+var y= 19 + 10
+console.log(y)
+
+var y= 25 + 11
+var y= 21 + 10
+console.log(y)
