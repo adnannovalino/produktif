afa6c3ee5ad6e4d7c0d12d9d2600cd05970dffa0
adnannovalino <168519055+adnannovalino@users.noreply.github.com>
2024-05-13 14:12:22 +0700

    Create tugas3.js

---
+++ tugas3.js
@@ -0,0 +1,35 @@
+//kondisi 1//
+const usia = 17;
+if (usia >= 18) {
+    console.log("Anda sudah dewasa.");
+} else {
+    console.log("Anda masih di bawah umur.");
+}
+
+const nilai= 85;
+if (nilai >= 75) {
+    console.log("Anda lulus.");
+} else {
+    console.log("Anda tidak lulus.");
+}
+
+const jam_tidur = 22_00;
+if (jam_tidur >= 20_00) {
+    console.log("selamat malam.");
+} else {
+    console.log("waktunya anda tidur.");
+}
+
+const suhu = 20;
+if (suhu >= 35) {
+    console.log("panas.");
+} else {
+    console.log("dingin.");
+}
+
+const berat_badan= 45;
+if (berat_badan>= 52) {
+    console.log("ideal.");
+} else {
+    console.log("kurus.");
+}
